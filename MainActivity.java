7c3779efb6a1c8b376541746ece08e97891a9d9e
lauragraham <legraha2@illinois.edu>
2018-04-22 19:27:58 -0500

    Setting up UI

--- MainActivity.java
+++ MainActivity.java
@@ -33,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         mImageUrls.add("https://cdn.pixabay.com/photo/2017/09/14/16/28/apple-pie-2749548_1280.jpg");
         mNames.add("Delicious Pie");
 
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mNames.add("Frozen Lake");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mNames.add("Washington");
+
         initRecyclerView();
     }
 
