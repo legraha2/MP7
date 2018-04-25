e87f52786083a99735e0fb9470dbf6a333754cad
lauragraham <legraha2@illinois.edu>
2018-04-25 16:26:44 -0500

    attempt to save user input, some other stuff

--- GalleryActivity.java
+++ GalleryActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+
 public class GalleryActivity extends AppCompatActivity {
     private static final String TAG = "GalleryActivity";
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
